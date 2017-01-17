c48b2a91026b6ae8071e36191b4fa894251609a0
donghabahn <donghabahn@berkeley.edu>
2017-01-17 11:16:45 +0900

    11:16

--- examples1617.c
+++ examples1617.c
@@ -4,12 +4,12 @@ int main(){
     double width=5,height=3;
     double area;
     double math=80,eng=60,physics=90,avg,sum;
-    area=width*height/2.0;
+    area=width*height/2;
     printf("AREA: %.1f\n",area);
 
 
     sum=math+eng+physics;
-    avg=sum/3.0;
+    avg=sum/3;
     printf("SUM: %.2f, AVERAGE: %.2f\n",sum,avg);
     return 0;
 }
